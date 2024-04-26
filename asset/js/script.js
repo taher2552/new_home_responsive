592125a992126f9e5cd3b6c4a44fa8b7617b6ea6
taher2552 <stardom.taher@gmail.com>
2024-04-26 11:40:21 +0530

    changes

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,34 +7,34 @@ $(document).ready(function(){
   function getRandomDigit() {
     return Math.floor(Math.random() * 10);
   }
-
-  // Function to roll the numbers until the target number is reached
+  
   function rollNumbers(targetNumber, duration, resultId) {
     const digits = $(`#${resultId} .digit`);
     const interval = duration / (targetNumber - 0); // Adjust duration for smoother animation
-
+  
     const intervalId = setInterval(() => {
       // Generate random digits for each position
       digits.each(function(){
         $(this).text(getRandomDigit());
       });
     }, 50); // Adjust the interval for smoother animation
-
+  
     // Update the final number after the specified duration
     setTimeout(() => {
       clearInterval(intervalId);
-      const finalDigits = String(targetNumber).padStart(5, '0').split('');
+      const finalDigits = String(targetNumber).padStart(digits.length, '0').split('');
       digits.each(function(index){
         $(this).text(finalDigits[index]);
       });
     }, duration);
   }
-
+  
   // Call the function with the target numbers and durations
-  rollNumbers(12520, 900, "players");
-  rollNumbers(14854, 900, "academies");
-  rollNumbers(14750, 900, "coaches");
-  rollNumbers(11485, 900, "teams");
+  rollNumbers(18000, 900, "players");
+  rollNumbers(28000, 900, "academies");
+  rollNumbers(67000, 900, "coaches");
+  rollNumbers(3800, 900, "teams");
+  
 
 //  register 
   const registerButton = $('#openRegester');
